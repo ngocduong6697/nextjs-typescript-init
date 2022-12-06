b1f557ac11d8b5b02e630c4cc4c4af801512b354
NGOC DUONG <ngocduong6697@gmail.com>
2022-12-06 15:15:35 +0700

    init project

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
-const nextTranslate = require("next-translate");
-const path = require("node:path");
+const nextTranslate = require('next-translate')
+const path = require('node:path')
 
-const nextConfig = {
+const nextConfig = nextTranslate({
   reactStrictMode: true,
   swcMinify: true,
   // config alias import
   webpack(config) {
-    return config;
+    return config
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
   env: {
     BASE_URL: process.env.BASE_URL,
     API_URL: process.env.API_URL,
     COOKIE_EXPIRED_DAYS: process.env.COOKIE_EXPIRED_DAYS,
   },
-};
+})
 
-module.exports = nextConfig;
+module.exports = nextConfig
