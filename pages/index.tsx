5e6ddfcd768e1dc70d4adcb0b05031894db4d20e
NGOC DUONG <ngocduong6697@gmail.com>
2022-12-06 15:36:54 +0700

    init project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,5 @@
-import styles from '/styles/Home.module.scss'
+import HomePage from '@app/components/HomePage'
 
-export default function Home() {
-  return (
-    // <div className={styles.container}>
-    <div>
-      <h1 className={styles.title}>My Title</h1>
-      <h1>Welcome to Next.js SASS example</h1>
-      <ul>
-        <li>
-          <a href="#">Link1</a>
-        </li>
-        <li>
-          <a href="#">Link2</a>
-        </li>
-        <li>
-          <a href="#">Link3</a>
-        </li>
-      </ul>
-    </div>
-  );
-}
+const IndexPage = () => <HomePage />
+
+export default IndexPage
